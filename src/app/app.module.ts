8ad59656eed8199a055f93e7bd6c15732f635ba4
Javier Gonzalez <jgonzalez@jgonzalez2019.local>
2020-03-16 16:34:17 -0300

    Basic routes scafolding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PeopleComponent } from './people/people.component';
+import { MoviesComponent } from './movies/movies.component';
+import { PlanetsComponent } from './planets/planets.component';
+
+const appRoutes: Routes = [
+   { path: 'movies', component: MoviesComponent},
+   { path: 'people', component: PeopleComponent},
+   { path: 'planets', component: PlanetsComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PeopleComponent,
+    MoviesComponent,
+    PlanetsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+       appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
